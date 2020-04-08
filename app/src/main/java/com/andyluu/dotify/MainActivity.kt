4759843e3eacd0d54a21ecda41db46fdb9c6bf09
Andy Luu <aluu5675@uw.edu>
2020-04-08 04:36:10 -0700

    Steps 5 to 7 Complete

--- app/src/main/java/com/andyluu/dotify/MainActivity.kt
+++ app/src/main/java/com/andyluu/dotify/MainActivity.kt
@@ -2,11 +2,24 @@ package com.andyluu.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnChangeUser.setOnClickListener {
+
+        }
+    }
+
+    fun changeUsername(view: View) {
+        val username = findViewById<TextView>(R.id.username)
+        username.text = "fdsfds"
     }
 }
