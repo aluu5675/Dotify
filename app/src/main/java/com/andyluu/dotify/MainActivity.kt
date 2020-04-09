c9ff0e9e580393f5c8df1e7de37282fbf1800595
Andy Luu <aluu5675@uw.edu>
2020-04-08 19:47:59 -0700

    Steps 8 to 9 Complete

--- app/src/main/java/com/andyluu/dotify/MainActivity.kt
+++ app/src/main/java/com/andyluu/dotify/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,13 +15,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var randomNumber = Random.nextInt(1000, 100000)
+        var numPlays = findViewById<TextView>(R.id.numPlays)
+        numPlays.text = "${randomNumber.toString()} plays"
+
+        btnPlay.setOnClickListener {
+            randomNumber += 1
+            numPlays.text = "${randomNumber.toString()} plays"
+        }
+
         btnChangeUser.setOnClickListener {
+            val username = findViewById<TextView>(R.id.username)
+            username.visibility = View.INVISIBLE
 
         }
+
+
     }
 
-    fun changeUsername(view: View) {
-        val username = findViewById<TextView>(R.id.username)
-        username.text = "fdsfds"
+    fun nextClicked(view: View) {
+        Toast.makeText(this, "Skipping to next track", Toast.LENGTH_SHORT).show()
+    }
+
+    fun prevClicked(view: View) {
+        Toast.makeText(this, "Skipping to previous track", Toast.LENGTH_SHORT).show()
     }
 }
